7f68c8fd9ea73215d1b18e688f2903234f51f939
Jake Van Alstyne <jakeva@gmail.com>
2014-03-06 21:16:47 -0700

    fixed some issues in 10.9

--- src/pwcheck.cpp
+++ src/pwcheck.cpp
@@ -69,12 +69,18 @@ int main(int argc, char* argv[])
     // These cryptographic values belong to a wallet that was successfully cracked by Dave Bitcoin (http://walletrecoveryservices.com)(http://www.reddit.com/r/Bitcoin/comments/1q8zan/)
     // The password that successfully unlocks these values is 'coffee-lemon', without the quotes
     // There was previously a reward here for anyone who cracked the password, but the reward has been claimed by Dave Bitcoin
-    const unsigned int nDeriveIterations = 90367;
-    const std::vector<unsigned char> chSalt = Convert("693f87a08e771ef0");
-    const std::vector<unsigned char> vchCryptedKey = Convert("153eabea2afe4fa960c4d0cc05eb8b233f7f00bc4dc55bebf129f40cdcc3bec3451d572c0851fa5327d08d532f48bbd5");
-    const std::vector<unsigned char> vchPubKey = Convert("04ffa71a0b3024136808c2e131a640e30b9c78812699e4cfa3b38a846207dcded9eade2549378c861e38343873ef9a94c6b54660c8aa0bd20146add7a0df7b4543");
-    const std::vector<unsigned char> vchCryptedSecret = Convert("6c88d9b67f5b3b0cb7390da96b1eeb04e520e3add8c8370959d045f5bb185a39fbea6fddd4f67a665c599e7620d4b9ce");
+    // const unsigned int nDeriveIterations = 90367;
+    // const std::vector<unsigned char> chSalt = Convert("693f87a08e771ef0");
+    // const std::vector<unsigned char> vchCryptedKey = Convert("153eabea2afe4fa960c4d0cc05eb8b233f7f00bc4dc55bebf129f40cdcc3bec3451d572c0851fa5327d08d532f48bbd5");
+    // const std::vector<unsigned char> vchPubKey = Convert("04ffa71a0b3024136808c2e131a640e30b9c78812699e4cfa3b38a846207dcded9eade2549378c861e38343873ef9a94c6b54660c8aa0bd20146add7a0df7b4543");
+    // const std::vector<unsigned char> vchCryptedSecret = Convert("6c88d9b67f5b3b0cb7390da96b1eeb04e520e3add8c8370959d045f5bb185a39fbea6fddd4f67a665c599e7620d4b9ce");
 
+    // Just a test with 0.8.6 created wallet, pw is 'password'
+    const unsigned int nDeriveIterations = 41407;
+    const std::vector<unsigned char> chSalt = Convert("ae936e1e2b61f19d");
+    const std::vector<unsigned char> vchCryptedKey = Convert("5ccae2184b30b910c0be835d583d1d3e4ed5046adec446a6f65dda4118e843a99f8bd61a05099ea411fb08a8c0076305");
+    const std::vector<unsigned char> vchPubKey = Convert("03fe4213051eb3fe64ceb3c27908e7198363cc6fa4827beef681df86d0d357578d");
+    const std::vector<unsigned char> vchCryptedSecret = Convert("020fcdd4c5cbd03ee300d65db222ca1fd2ea59c5313e1472913cf2bffda281eb053d68f438f3667b901621a726ee69fa");
 
 
     // Try any password as input
